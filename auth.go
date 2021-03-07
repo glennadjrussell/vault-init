80e5b4ec63e4eb05947e36b635485f50a08743ed
Glenn Russell <grussell@qarik.com>
2021-03-07 14:42:01 +0000

    Begin adding policy creation

--- auth.go
+++ auth.go
@@ -22,13 +22,17 @@ type GithubMapTeamRequest struct {
 	Policies []string `json:"value"`
 }
 
+type PolicyCreateRequest struct {
+	Policy string `json:"policy"`
+}
+
 func mapGithubTeam(key string) (bool, error) {
 }
 
 //
 // {"type":"github","description":"","config":{"options":null,"default_lease_ttl":"0s","max_lease_ttl":"0s","force_no_cache":false},"local":false,"seal_wrap":false,"external_entropy_access":false,"options":null}
 //
-func enableGithubAuth(key string) (bool, error) {
+func enableGithubAuth(token string) (bool, error) {
 	authRequest := GithubAuthRequest{
 	}
 
@@ -58,5 +62,45 @@ func enableGithubAuth(key string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+}
+
+func createPolicy(token string, key string, policy string) (bool, error) {
+	policyRequest := PolicyRequest{
+		Policy: policy
+	}
+
+	policyRequestData, err := json.Marshal(&policyRequest)
+	if err != nil {
+		return false, err
+	}
+
+	r := bytes.NewReader(policyRequestData)
+	request, err := http.NewRequest(http.MethodPut,  vaultAddr+"/v1/sys/policy/"+key, r)
+
+	response, err := httpClient.do(request)
+	if err != nil {
+		return false, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return false, fmt.Errorf("policy creation: non-200 status code: %d", response.StatusCode)
+	}
+
+	return true, nil
+}
+
+func initializeAuth(token string) (bool, err) {
+	res, err := createPolicy(token, "vault_admins", "/policies/admin.hcl")
+	if err != nil {
+		return false, err
+	}
+
+	res, err := enableGithubAuth(token)
+	if err != nil {
+		return false, err
+	}
+
+	return false, nil
 }
 
